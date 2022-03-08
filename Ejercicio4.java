5955f61319c339c4b2837987a7ac4a7e7840c2fd
FlavioAngelini <97698028+FlavioAngelini@users.noreply.github.com>
2022-03-08 19:23:06 +0100

    Add files via upload

---
+++ Ejercicio4.java
@@ -0,0 +1,20 @@
+package Tarea4;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+		/**
+		 * Prev:
+		 * Post:
+		 */
+	public static void main(String[] arg) {
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("escribe un número");
+		int n = entrada.nextInt();
+		System.out.println("escribe otro número");
+		int x = entrada.nextInt();	
+		for (n = n + 1; n < x ; n++) {
+		System.out.println("estos son los numeros entre los elegidos= " + n );
+		}
+	}
+}
